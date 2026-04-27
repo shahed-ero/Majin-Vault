4c43e185435ab66a20138bcd81ee7371dfd46c13
Shahed <anbxybkt@gmail.com>
2026-04-28 00:04:07 +0600

    saparating js to a file app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+      document.getElementById("trs").addEventListener("click", function () {
+        document.getElementById("prank").textContent =
+          "PRANK Congratulations! You've WON Iphone17 PRANK!";
+      });
