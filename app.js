0eb471d35760f88cd0b215a6488363a3e485c813
Shahed <anbxybkt@gmail.com>
2026-05-03 22:16:04 +0600

    image and hover me hover out effects

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-      document.getElementById("trs").addEventListener("click", function () {
-        document.getElementById("prank").textContent =
-          "PRANK Congratulations! You've WON Iphone17 PRANK!";
-      });
+//works for click to change the image into 1.jpg
+const btn = document.getElementById('imgShow');
+const image = document.getElementById('majinImg');
+function revelLittleMajin() {
+    image.src = '1.jpg';
+} 
+btn.addEventListener('click', revelLittleMajin);
+//works end here
+
+
+
+
+/*this is where for hover me and hover out
+so wath this
+*/
+
+
+//Hover me start here
+const hvrtxt = document.getElementById('hoverText');
+function hoverText() {
+    hvrtxt.innerHTML = 'You hovered over the text!';
+}
+hvrtxt.addEventListener('mouseover', hoverText);
+//hover me end here
+
+
+
+//hover out start here
+function hoverOutText() {
+    hvrtxt.innerHTML = 'Hover over me!';
+}
+hvrtxt.addEventListener('mouseout', hoverOutText);
+//hover out end here
